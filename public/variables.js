bb21040eb384deea600246c49ecbf13f39ba15a3
Ahmad Mahboob <140995629+Ahmad-Mahboob@users.noreply.github.com>
2024-12-11 20:30:07 +0500

    Update variables.js

--- public/variables.js
+++ public/variables.js
@@ -23,7 +23,7 @@ let quesNo = 0;
 
 async function fetchQuestions() {
     try {
-        const response = await fetch('http://localhost:5000/api/questions');
+        const response = await fetch('mongodb+srv://admin:Ahmad5568@@cluster0.ncheo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         const questions = await response.json();
         quizQuestions = questions;
         actualAnswers = questions.map(q => q.correctAnswer);
