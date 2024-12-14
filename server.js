221095d8669aec9ee33e97e5f3d3cf52aba34949
Ahmad Mahboob <140995629+Ahmad-Mahboob@users.noreply.github.com>
2024-12-14 21:11:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/ahmad'(req,res) => {
+res.write("Hello Ahmad")
+    res.end()
+})
 
 
 
